4b02f3eb89fc6c24b2e00e82decc3daeb5edd2b3
SusanYingshuChen <yingshu2008@gmail.com>
2022-07-26 19:41:50 +0800

    set up project page

---
+++ interactive_viewer/scenes/i2i-translation-1/data.js
@@ -0,0 +1,181 @@
+const data =
+{
+    "imageBoxes": [
+        {
+            "title": "GOLDEN",
+            "elements": [
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "Reference.png"
+                },
+                {
+                    "title": "Reference Style",
+                    "version": "-",
+                    "image": "Reference Style_Golden.png"
+                },
+                {
+                    "title": "Ours",
+                    "version": "-",
+                    "image": "Ours_Golden.png"
+                },
+                {
+                    "title": "Ours-opt",
+                    "version": "-",
+                    "image": "Ours-opt_Golden.png"
+                },
+                {
+                    "title": "DRIT++",
+                    "version": "-",
+                    "image": "DRIT++_Golden.png"
+                },
+                {
+                    "title": "MUNIT",
+                    "version": "-",
+                    "image": "MUNIT_Golden.png"
+                },
+                {
+                    "title": "FUNIT",
+                    "version": "-",
+                    "image": "FUNIT_Golden.png"
+                },
+                {
+                    "title": "DSMAP",
+                    "version": "-",
+                    "image": "DSMAP_Golden.png"
+                },
+                {
+                    "title": "StarGANv2",
+                    "version": "-",
+                    "image": "StarGANv2_Golden.png"
+                }
+            ]
+        },
+        {
+            "title": "BLUE",
+            "elements": [
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "Reference.png"
+                },
+                {
+                    "title": "Reference Style",
+                    "version": "-",
+                    "image": "Reference Style_Blue.png"
+                },
+                {
+                    "title": "Ours",
+                    "version": "-",
+                    "image": "Ours_Blue.png"
+                },
+                {
+                    "title": "Ours-opt",
+                    "version": "-",
+                    "image": "Ours-opt_Blue.png"
+                },
+                {
+                    "title": "DRIT++",
+                    "version": "-",
+                    "image": "DRIT++_Blue.png"
+                },
+                {
+                    "title": "MUNIT",
+                    "version": "-",
+                    "image": "MUNIT_Blue.png"
+                },
+                {
+                    "title": "FUNIT",
+                    "version": "-",
+                    "image": "FUNIT_Blue.png"
+                },
+                {
+                    "title": "DSMAP",
+                    "version": "-",
+                    "image": "DSMAP_Blue.png"
+                },
+                {
+                    "title": "StarGANv2",
+                    "version": "-",
+                    "image": "StarGANv2_Blue.png"
+                }
+            ]
+        },
+        {
+            "title": "NIGHT",
+            "elements": [
+                {
+                    "title": "Input",
+                    "version": "-",
+                    "image": "Reference.png"
+                },
+                {
+                    "title": "Reference Style",
+                    "version": "-",
+                    "image": "Reference Style_Night.png"
+                },
+                {
+                    "title": "Ours",
+                    "version": "-",
+                    "image": "Ours_Night.png"
+                },
+                {
+                    "title": "Ours-opt",
+                    "version": "-",
+                    "image": "Ours-opt_Night.png"
+                },
+                {
+                    "title": "DRIT++",
+                    "version": "-",
+                    "image": "DRIT++_Night.png"
+                },
+                {
+                    "title": "MUNIT",
+                    "version": "-",
+                    "image": "MUNIT_Night.png"
+                },
+                {
+                    "title": "FUNIT",
+                    "version": "-",
+                    "image": "FUNIT_Night.png"
+                },
+                {
+                    "title": "DSMAP",
+                    "version": "-",
+                    "image": "DSMAP_Night.png"
+                },
+                {
+                    "title": "StarGANv2",
+                    "version": "-",
+                    "image": "StarGANv2_Night.png"
+                }
+            ]
+        }
+    ],
+    "stats": [
+        {
+            "title": "Stats",
+            "labels": [
+                "Reference Style",
+                "Ours",
+                "Ours-opt",
+                "DRIT++",
+                "MUNIT",
+                "FUNIT",
+                "DSMAP",
+                "StarGANv2"
+            ],
+            "series": []
+        },
+        {
+            "title": "Stats",
+            "labels": [],
+            "series": []
+        },
+        {
+            "title": "Stats",
+            "labels": [],
+            "series": []
+        }
+    ]
+}
